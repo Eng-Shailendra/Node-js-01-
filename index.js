26401b8306cccd1222d447b761edec35c9bb2d92
shailendra sahu <shailendra2130@gmail.com>
2026-04-21 20:41:37 +0530

    intoduction of NodeJs and its features

---
+++ index.js
@@ -0,0 +1,12 @@
+//! Node Js --> NodeJS is run time enviorment that let you run javascript on the serverside , out of the browser
+
+
+//? MODULE -- a module is logical pice of isolated code by keeping code inside a module,
+// you protect its variable and function from leaking into other parts of your app,
+// preventn naming conflicts.
+
+//!  type of module
+
+// ! 1) built in module
+// ! 2) user defined module
+// ! 3) third-party module
