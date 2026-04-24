ac415662f3697e9af2f7ec053f1ea4c6979d4048
shailendra sahu <shailendra2130@gmail.com>
2026-04-24 09:31:38 +0530

    built in module  i compleated

---
+++ 0_module/Third-party-Module/Mongodb/index.js
@@ -0,0 +1,18 @@
+import mongodb from "mongodb";
+
+async function connectDB() {
+
+    // step 1 -- create a connection 
+    let client = await mongodb.MongoClient.connect("mongodb://localhost:27017");
+
+    // step 2 -- to create database 
+    let database = client.db("PRACTICE_DB");
+
+    // step 3 -- to create collection
+    let User = await database.collection("User");
+
+
+
+
+
+}
